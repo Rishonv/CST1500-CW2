f0e9130bcc9ae9ca2526c2e676a1a6ea7e242016
Kartik Nair <kartiknair1911@gmail.com>
2020-01-22 13:49:21 +0400

    Rewrote menu

--- cw2.sh
+++ cw2.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-MENU=`zenity --list --title="What would you like to do?" --column="Options"\
-    "Date & Time" \
-    "Calendar" \
-    "Delete" \
-    "Exit"`
+menu=("Date & Time" "Calendar" "Delete" "Exit")
+answer=`zenity --list --column=Menu "${menu[@]}" --height 170`
 
-case $? in
-    0) zenity --info --title="Date & Time" --width=280 --height=100 \
+case "$answer" in
+    "Date & Time") zenity --info --title="Date & Time" --width=280 --height=100 \
         --text="The system date &amp; time is: \n$(date)";;
-    1) zenity --calendar;;
-    2) FILE=`zenity --file-selection --title="Select a File"`
+    "Calendar") zenity --calendar;;
+    "Delete") FILE=`zenity --file-selection --title="Select a File"`
         case $? in
          0) if zenity --question --width=280 --height=100 --title="Are you sure?" --text="Are you sure you would like to delete \"$FILE\"?"
             then 
@@ -19,8 +16,7 @@ case $? in
             else
                 zenity --info --width=280 --height=100 --title="Delete unsuccessful" --text="File not deleted."
             fi;;
-         1) zenity --info --width=280 --height=100 --title="No file" --text="No file selected.";;
         -1) zenity --error --width=280 --height=100 --title="Error" --text="An unexpected error occurred";;
         esac;;
-    3) exit;;
+    "Exit") exit;;
 esac;
